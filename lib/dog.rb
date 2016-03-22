135288a338425e45c5aebba10da786d569cb9f65
Saul Ocampo <digitalh2o@gmail.com>
2016-03-22 02:32:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  
+  def initialize(name, breed = "Mut")
+    @name = name
+    @breed = breed
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  
+  
+end
